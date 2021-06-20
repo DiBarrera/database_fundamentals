df73ab3096969cd86d821031388b1bd7b0554092
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-20 14:36:29 -0500

    ASKING TO THE DATABASE

---
+++ SELECT_COUNT_FROM_INNER_JOIN_INNER_JOIN_GROUP_BY.sql
@@ -0,0 +1,6 @@
+SELECT posts.titulo, COUNT(*) num_etiquetas
+FROM posts
+	INNER JOIN posts_etiquetas ON posts.id = posts_etiquetas.post_id
+    INNER JOIN etiquetas ON etiquetas.id = posts_etiquetas.etiqueta_id
+GROUP BY posts.id
+ORDER BY num_etiquetas DESC;
---
+++ SELECT_FROM_LEFT_JOIN_WHERE.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM etiquetas
+	LEFT JOIN posts_etiquetas ON etiquetas.id = posts_etiquetas.etiqueta_id
+WHERE posts_etiquetas.etiqueta_id IS NULL;
